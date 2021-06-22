fefd18954a5d92e879352eea4aa13cd9c9ab4466
jjia0038 <jjia0038@student.monash.edu>
2021-06-22 14:17:07 +1000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { identity } from "svelte/internal";
+	import Login from "./Components/Login.svelte"
+</script>
+
+<main>
+	<Login/>
+</main>
+
+<style>
+
+</style>
---
+++ src/Components/Login.svelte
@@ -0,0 +1,37 @@
+<script>
+    let username;
+    let password;
+    let authenticate;
+
+    const btnClick = () => {
+        console.log("potato");
+    }
+</script>
+
+<form>
+    <div class = "group">
+        <label for="username">Enter your user name here</label>
+        <input type = "text" bind:value={username}>
+    </div>
+
+    <div class = "group">
+        <label for="password">Enter your password here</label>
+        <input type = "text" bind:value={password}>
+    </div>
+
+    <div class = "group">
+        <label for = "authenticate">Enter your authenticate key here</label>
+        <input type = "text" bind:value={authenticate}>
+    </div>
+</form>
+
+<style>
+    .group {
+        display: flex;
+        margin-bottom: 10px;
+    }
+    label {
+        float: left;
+        display: block;
+    }
+</style>
