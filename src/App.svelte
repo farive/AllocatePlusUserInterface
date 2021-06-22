291ef256060f6180bb075d837f04e22f4f9938c0
jjia0038 <jjia0038@student.monash.edu>
2021-06-22 15:39:24 +1000

    Created an interface for users to login

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
-	import { identity } from "svelte/internal";
 	import Login from "./Components/Login.svelte"
+
+	let loginInfo = {
+		username: "",
+		password: "",
+		authenticate: 0
+	}
+
+	const handleLoginInfo = (event) => {
+		loginInfo["username"] = event.detail["username"];
+		loginInfo["password"] = event.detail["password"];
+		loginInfo["authenticate"] = event.detail["authenticate"];
+	}
 </script>
 
 <main>
-	<Login/>
+	<div class = "Login">
+		<Login on:handleLoginInfo={handleLoginInfo}/>
+	</div>
 </main>
 
 <style>
-
+	.Login{
+		display: flex;
+		justify-content: left;
+	}
 </style>
--- src/Components/Login.svelte
+++ src/Components/Login.svelte
@@ -1,37 +1,51 @@
 <script>
+
+    import {createEventDispatcher} from "svelte";
+
+    let dispatch = createEventDispatcher();
     let username;
     let password;
     let authenticate;
 
-    const btnClick = () => {
-        console.log("potato");
+    const handleSubmit = () => {
+        const loginInfo = {
+            username: username,
+            password: password,
+            authenticate: authenticate
+        }
+        dispatch("handleLoginInfo", loginInfo);
     }
 </script>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
+    <h1>Login</h1>
     <div class = "group">
-        <label for="username">Enter your user name here</label>
-        <input type = "text" bind:value={username}>
+        <input type = "text" placeholder="Username" bind:value={username}>
     </div>
 
     <div class = "group">
-        <label for="password">Enter your password here</label>
-        <input type = "text" bind:value={password}>
+        <input type = "text" placeholder="Password" bind:value={password}>
     </div>
 
     <div class = "group">
-        <label for = "authenticate">Enter your authenticate key here</label>
-        <input type = "text" bind:value={authenticate}>
+        <input type = "text" placeholder = "Authenticate Key" bind:value={authenticate}>
     </div>
+    <button>Login</button>
 </form>
 
 <style>
-    .group {
+    form {
         display: flex;
-        margin-bottom: 10px;
+        flex-direction: column;
+        align-items: center;
+        border: 1px black solid;
+        padding: 10px;
+        width: fit-content;
     }
-    label {
-        float: left;
-        display: block;
+
+    input {
+        border: 0px none;
+        margin: 10px;
+        width: fit-content;
     }
 </style>
